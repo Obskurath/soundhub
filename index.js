8cdb6d62fb124b1b3bca9e54d3c37b82bd8bbcd3
Eduardo <EduardoVG@proton.me>
2024-11-18 20:51:12 -0600

    Implement Discord bot with command handling and player integration

---
+++ index.js
@@ -0,0 +1,77 @@
+require("dotenv").config();
+
+const {REST} = require("@discordjs/rest");
+const {Routes} = require("discord-api-types/v9");
+const {Client, GatewayIntentBits, Collection, Guild, Message, VoiceState} = require("discord.js");
+const {Player} = require("discord-player");
+
+const fs = require("node:fs");
+const path = require("node:path");
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildVoiceStates
+    ]    
+});
+
+// LOAD ALL THE SLASH COMMANDS (/)
+const commands = [];
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, "commands");
+const commandsFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+
+for (const file of commandsFiles){
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    client.commands.set(command.data.name, command);
+    commands.push(command);
+}
+
+// PLAYER 
+
+client.player = new Player(client, {
+    ytdlOptions: {
+        quality: "highestaudio",
+        highWaterMark: 1 << 25
+    }
+});
+
+
+client.on("ready", () => {
+    const guild_ids = client.guilds.cache.map(guild => guild.id);
+
+    const rest = new REST({version: "9"}).setToken(process.env.DISCORD_TOKEN);
+    for (const guildId of guild_ids)
+
+        {
+            rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, guildId),{
+                body: commands
+            })
+            .then(() => console.log(`Added commands to ${guildId}`))
+            .catch(console.error)
+        }
+});
+
+
+client.on("interactionCreate", async interaction => {
+    if(!interaction.isCommand()) return;
+
+    const command= client.commands.get(interaction.commandName);
+    if(!command) return;
+
+    try{
+        await command.execute({client, interaction});
+    }
+    catch(err)
+    {
+        console.error(err);
+        await interaction.reply("An error occured while executing that command")
+    }
+    
+});
+
+client.login(procces.env.DISCORD_TOKEN)
