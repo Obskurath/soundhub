f84c1ca48c400d515fb4051117025c262be2a230
Eduardo <EduardoVG@proton.me>
2024-11-18 21:42:16 -0600

    Add resume command to allow users to resume the current song

---
+++ commands/resume.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("resume")
+        .setDescription("Resumes the current song."),
+    execute: async ({client, interaction}) => {
+        const queue = client.player.getQueue(interaction.guild);
+
+        if(!queue) {
+            await interaction.reply("There is no song playing");
+            return;
+        }
+
+       queue.setPaused(false);
+
+       await interaction.reply("Resumed playing");
+
+    }
+}
