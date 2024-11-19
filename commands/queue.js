8d5f9218891c58f2da3dd6b1930ef605340f226c
Eduardo <EduardoVG@proton.me>
2024-11-19 04:34:49 -0600

    Refactor queue command to improve error handling and enhance output formatting

--- commands/queue.js
+++ commands/queue.js
@@ -5,30 +5,36 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName("queue")
         .setDescription("Shows the first 10 songs in the queue."),
-    execute: async ({client, interaction}) => {
-        const queue = client.player.queues.get(interaction.guild);
+    execute: async ({ client, interaction }) => {
+        const queue = client.player.queues.get(interaction.guild.id);
 
-        if(!queue || !queue.playing) {
+        if (!queue || !queue.playing) {
+            console.log("No queue or no song playing.");
             await interaction.reply({
-                content: "There is no current playlist",
-                ephemeral: true
-            })
+                content: "There is no current playlist.",
+                ephemeral: true,
+            });
             return;
         }
 
-        const queueString = queue.tracks.slice(0,10).map((song, i) => {
-            return `${i + 1}) [${song.duration}]\` ${song.title} - <@${song.requestBy.id}>`;
-        }).join("\n")
+        const queueString = queue.tracks
+            .slice(0, 10)
+            .map((song, i) => {
+                return `${i + 1}) ${song.title}`;
+            })
+            .join("\n");
 
         const currentSong = queue.current;
 
-        await interaction.reply({
-            embeds : [
-                new EmbedBuilder()
-                .setDescription(`Currently playing: \n\` ${currentSong.title} - <@${currentSong.requestBy.id}>\n\n Queue:\n${queueString}`)
-                .SetThumbnail(currentSong.thumbnail)
-            ]
-        })
-    }
+        console.log("Current song:", currentSong);
+        console.log("Queue tracks:", queue.tracks);
 
-}
+        await interaction.reply({
+            embeds: [
+                new EmbedBuilder()
+                    .setDescription(`Currently playing: ${currentSong.title}\n\nQueue:\n${queueString}`)
+                    .setThumbnail(currentSong.thumbnail),
+            ],
+        });
+    },
+};
