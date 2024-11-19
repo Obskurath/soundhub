bc3e884529af79ee264fadd3308d8f308d004fa9
Eduardo <EduardoVG@proton.me>
2024-11-18 21:44:11 -0600

    Add exit command to leave the voice channel and notify the user

---
+++ commands/exit.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("exit")
+        .setDescription("Exits the voice channel."),
+    execute: async ({client, interaction}) => {
+        const queue = client.player.getQueue(interaction.guild);
+
+        if(!queue) {
+            await interaction.reply("There is no song playing");
+            return;
+        }
+
+       queue.destroy();
+
+       await interaction.reply("Why you bully me? :c")
+
+    }
+}
