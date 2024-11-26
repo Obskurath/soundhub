a3193bb616c13b205136cc124e9534a14211381d
Eduardo Valencia <133633137+Obskurath@users.noreply.github.com>
2024-11-26 03:06:43 -0600

    feat: Update button interaction ID in play command to 'resume'

--- commands/play.js
+++ commands/play.js
@@ -82,7 +82,7 @@ module.exports = {
         const row = new ActionRowBuilder()
             .addComponents(
                 new ButtonBuilder()
-                    .setCustomId('play')
+                    .setCustomId('resume')
                     .setEmoji('▶️')
                     .setStyle(ButtonStyle.Secondary),
                 new ButtonBuilder()
